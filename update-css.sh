6d258aa94e901f0bd8c4dcf78a4cb7ac5a154ac8
Igor Baiborodine <igor@kiroule.com>
2023-06-28 09:41:49 -0400

    feat(ci): update theme.css in "Build" step deploying to GitHub Pages

--- update-css.sh
+++ update-css.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i "s~webfonts~bilberry-hugo-theme-sandbox/webfonts~g" public/theme.min.*.css
-sed -i "s~/fonts~/bilberry-hugo-theme-sandbox/fonts~g" public/theme.min.*.css
+sed -i "s~webfonts~bilberry-hugo-theme-sandbox/webfonts~g" public/theme.css
+sed -i "s~/fonts~/bilberry-hugo-theme-sandbox/fonts~g" public/theme.css
