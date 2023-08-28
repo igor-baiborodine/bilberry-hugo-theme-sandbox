677a31fa16e0b22fbc1cdde374668e249f69cf06
igor-baiborodine <igor@kiroule.com>
2023-08-28 17:12:46 -0400

    feat(ci): update deployment to GitHub Pages action

--- update-version.sh
+++ update-version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sed -i "s/title = .*$/title = \"Bilberry v3 Sandbox\"/" config.toml
-sed -i "s/theme = .*$/theme = \"github.com\/Lednerb\/bilberry-hugo-theme\/v3\"/" config.toml
+#sed -i "s/title = .*$/title = \"Bilberry v3 Sandbox\"/" config.toml
+sed -i "s/theme = .*$/theme = \"github.com\/Lednerb\/bilberry-hugo-theme\/v4\"/" config.toml
 hugo mod get
 
-version=$(grep -Po "(?<=v3\s)[^\s//]+" go.mod)
+version=$(grep -Po "(?<=v4\s)[^\s//]+" go.mod)
 sed -i "s/creditsText = .*$/creditsText = \"Bilberry Hugo Theme $version\"/" config.toml
 sed -i "s/creditsUrl = .*$/creditsUrl = \"https:\/\/github.com\/Lednerb\/bilberry-hugo-theme\/tree\/$version\"/" config.toml
