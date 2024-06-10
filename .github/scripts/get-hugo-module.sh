c41c3362e23edf71541253107f0f3b8748a54479
Igor Baiborodine <igor@kiroule.com>
2024-06-10 08:17:50 -0400

    feat: update GitHub actions (#49)

--- .github/scripts/get-hugo-module.sh
+++ .github/scripts/get-hugo-module.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#sed -i "s/title = .*$/title = \"Bilberry v3 Sandbox\"/" config.toml
-sed -i "s/theme = .*$/theme = \"github.com\/Lednerb\/bilberry-hugo-theme\/v4\"/" config.toml
+#sed -i "s/title = .*$/title = \"Bilberry v3 Sandbox\"/" hugo.toml
+sed -i "s/theme = .*$/theme = \"github.com\/Lednerb\/bilberry-hugo-theme\/v4\"/" hugo.toml
 hugo mod get
 
 version=$(grep -Po "(?<=v4\s)[^\s//]+" go.mod)
-sed -i "s/creditsText = .*$/creditsText = \"Bilberry Hugo Theme $version\"/" config.toml
-sed -i "s/creditsUrl = .*$/creditsUrl = \"https:\/\/github.com\/Lednerb\/bilberry-hugo-theme\/tree\/$version\"/" config.toml
+sed -i "s/creditsText = .*$/creditsText = \"Bilberry Hugo Theme $version\"/" hugo.toml
+sed -i "s/creditsUrl = .*$/creditsUrl = \"https:\/\/github.com\/Lednerb\/bilberry-hugo-theme\/tree\/$version\"/" hugo.toml
